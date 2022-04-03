33a01049ab4baa9a1a4a4269b307aba1469923c0
DimaPashchenko <kitaminchik@gmail.com>
2022-04-03 17:14:30 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -16,4 +16,25 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x*1 + 1;
 	window.localStorage.setItem(key, x);
+
+	//виводить  кіл-сть пицци у корзині
+	alert ('Pizza in your cart:' + cart_get_number_of_items())
+}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //отримаємо ключ
+		var value = window.localStorage.getItem(key); //отримаємо значення по ключу; in Ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
 }
