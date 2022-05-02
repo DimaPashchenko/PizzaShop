739d8bf50f0feb951d59efca5130b92484207805
DimaPashchenko <kitaminchik@gmail.com>
2022-05-02 16:47:26 +0300

    Save order to database, display "Thank you". clear LocalStorage

--- app.rb
+++ app.rb
@@ -10,6 +10,9 @@ set :database, {adapter: "sqlite3", database: "pizzashop.db"}
 class Product < ActiveRecord::Base
 end
 
+class Order < ActiveRecord::Base
+end
+
 get '/' do
 	@products = Product.all
 	erb :index
@@ -17,6 +20,11 @@ end
 
 get '/about' do
 	erb :about
+end
+
+post '/place_order' do
+	@order = Order.create params[:order]
+	erb :order_placed
 end
 
 post '/cart' do
